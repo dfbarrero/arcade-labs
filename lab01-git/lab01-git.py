fdb7f91c6d384da9a6da161d6bcb645eec074c10
David F. Barrero <david.barrero@gmail.com>
2026-01-22 19:38:35 +0100

    Render added

--- lab01-git/lab01-git.py
+++ lab01-git/lab01-git.py
@@ -3,8 +3,10 @@ import arcade
 WIDTH = 800
 HEIGHT = 600
 
-arcade.open_window(WIDTH, HEIGHT, "Ejemplo")
+arcade.open_window(WIDTH, HEIGHT, "Example")
 
-arcade.draw_text("Hola, mundo", 350, 300, arcade.color.WHITE)
+arcade.start_render()
+arcade.draw_text("Hello, world", 350, 300, arcade.color.WHITE)
+arcade.finish_render()
 
 arcade.run()
